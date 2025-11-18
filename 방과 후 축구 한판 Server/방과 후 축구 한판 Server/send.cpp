5735cb4febd3f7d9048a436fb8f25e7d22bb2400
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2025-11-18 13:07:42 +0900

    send_gameover() 한번에 보내도록 수정

--- 방과 후 축구 한판 Server/방과 후 축구 한판 Server/send.cpp
+++ 방과 후 축구 한판 Server/방과 후 축구 한판 Server/send.cpp
@@ -4,13 +4,9 @@
 void send_gameover(SOCKET socket) {
     PacketGameover gameover;
     gameover.gameover = true;
-    int sent1 = send(socket, (char*)&gameover.header, sizeof(PacketHeader), 0);
-    if (sent1 == sizeof(PacketHeader))
-        printf("error send_gameover.header");
-
-    int sent2 = send(socket, (char*)&gameover.gameover, gameover.header.size, 0);    // 제대로 동작하는가? -> 구조체 정의가 완전히 동일, Padding 없음, 서버와 클라는 같은 플랫폼, 엔디안이 같음.
-    if (sent2 == sizeof(gameover.header.size))
-        printf("error send_gameover.gameover");
+    int sent = send(socket, (char*)&gameover, sizeof(PacketGameover), 0);
+    if (sent == sizeof(PacketHeader))
+        std::cerr << "send_gameover()" << std::endl;
 }
 
 void send_renderdata(SOCKET socket, PacketRenderData& renderdata) {
