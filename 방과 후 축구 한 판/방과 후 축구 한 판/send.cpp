4489f93bfb71575eec95f702e52dc1f374602757
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2025-11-27 12:18:13 +0900

    input 보내는 방식을 변경 키다운 키업 때 한 번씩만 보내도록

--- 방과 후 축구 한 판/방과 후 축구 한 판/send.cpp
+++ 방과 후 축구 한 판/방과 후 축구 한 판/send.cpp
@@ -2,14 +2,20 @@
 
 // 조작(send_input)
 void PlayerInput(int key_value, PacketInputkey& key, SOCKET sock, bool toggle) {
-    key.key[key_value] = toggle;
-    int sent = send(sock, (char*)&key, sizeof(PacketInputkey), 0);
-    if (sent == SOCKET_ERROR)
-        std::cerr << "error: PlayerInput()" << std::endl;
+    if (key.key[key_value] != toggle) {
+        key.key[key_value] = toggle;
+        int sent = send(sock, (char*)&key, sizeof(PacketInputkey), 0);
+        if (sent == SOCKET_ERROR)
+            std::cerr << "error: PlayerInput()" << std::endl;
+        std::cout << "키 입력: " << key_value << std::endl;
+    }
 }
 void PlayerInput_special(int key_value, PacketInputspecialkey& key, SOCKET sock, bool toggle) {
-    key.key[key_value] = toggle;
-    int sent = send(sock, (char*)&key, sizeof(PacketInputspecialkey), 0);
-    if (sent == SOCKET_ERROR)
-        std::cerr << "error: PlayerInput_special()" << std::endl;
+    if (key.key[key_value] != toggle) {
+        key.key[key_value] = toggle;
+        int sent = send(sock, (char*)&key, sizeof(PacketInputspecialkey), 0);
+        if (sent == SOCKET_ERROR)
+            std::cerr << "error: PlayerInput_special()" << std::endl;
+        std::cout << "키 입력: " << key_value << std::endl;
+    }
 }
