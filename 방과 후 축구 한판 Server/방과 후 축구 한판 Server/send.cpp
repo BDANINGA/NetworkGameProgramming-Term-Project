ea7a70d875ff263a11dd09d6017f08b5f7dbfd7d
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2025-12-09 11:15:54 +0900

    Server send 함수 예외 처리 오류 수정

--- 방과 후 축구 한판 Server/방과 후 축구 한판 Server/send.cpp
+++ 방과 후 축구 한판 Server/방과 후 축구 한판 Server/send.cpp
@@ -5,14 +5,14 @@ void send_gameover(SOCKET& socket) {
     PacketGameover gameover;
     gameover.gameover = true;
     int sent = send(socket, (char*)&gameover, sizeof(PacketGameover), 0);
-    if (sent == sizeof(PacketGameover))
+    if (sent == SOCKET_ERROR)
         std::cerr << "error: send_gameover()" << std::endl;
 }
 
 // send_userdata - 11.19 ±¸Çö
 void send_userdata(SOCKET& socket, PacketUserData& userdata) {
     int sent = send(socket, (char*)&userdata, sizeof(PacketUserData), 0);
-    if (sent == sizeof(PacketUserData))
+    if (sent == SOCKET_ERROR)
         std::cerr << "error: send_userdata()" << std::endl;
 }
 
@@ -20,6 +20,6 @@ void send_loginresult(SOCKET& socket, PacketLoginResult& loginresult, bool resul
 	loginresult.success = result;
 	strcpy_s(loginresult.message, message);
     int sent = send(socket, (char*)&loginresult, sizeof(PacketLoginResult), 0);
-    if (sent == sizeof(PacketLoginResult))
+    if (sent == SOCKET_ERROR)
         std::cerr << "error: send_loginresult()" << std::endl;
 }
