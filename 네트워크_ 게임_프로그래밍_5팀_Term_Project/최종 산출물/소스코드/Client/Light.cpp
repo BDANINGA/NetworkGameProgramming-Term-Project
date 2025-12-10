0c473101ab34edfbc1147c07b7aad280edc94ac8
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2025-12-10 22:00:52 +0900

    파일 다시 정리 제출물 준비

--- 네트워크_ 게임_프로그래밍_5팀_Term_Project/최종 산출물/소스코드/Client/Light.cpp
+++
@@ -1,6 +0,0 @@
-#include "Light.h"
-
-glm::vec3 Light::getColor() { return this->color; }
-glm::vec3 Light::getPosition() { return this->position; };
-glm::vec3 Light::getViewPos() { return this->viewpos; };
-float Light::getAmbientlight() { return this->ambientlight; };
--- 네트워크_ 게임_프로그래밍_5팀_Term_Project/최종 산출물/소스코드/Server/Object.cpp
+++
@@ -1,7 +0,0 @@
-#include "Object.h"
-
-Object::Object() = default;
-glm::vec3 Object::getPosition() { return position; };
-float Object::getDistance(const Object &other) {
-	return glm::distance(this->position, other.position);
-}
